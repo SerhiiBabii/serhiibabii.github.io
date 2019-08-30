8e169d27c6a28505ed7338a916b80222e3a72eaf
Serhii Babii <babiysergey1988@gmail.com>
2019-08-30 12:38:20 +0300

    Added new react-projects and changed index.html

---
+++ react/form/build/precache-manifest.222a1106a5164ea22ce4298e45a3c136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7df55d8007d803f4545b",
+    "url": "/static/js/main.6cf27472.chunk.js"
+  },
+  {
+    "revision": "7bea679dfd9e2bf255ad",
+    "url": "/static/js/2.df704df5.chunk.js"
+  },
+  {
+    "revision": "7df55d8007d803f4545b",
+    "url": "/static/css/main.a7b9762e.chunk.css"
+  },
+  {
+    "revision": "7bea679dfd9e2bf255ad",
+    "url": "/static/css/2.d4fe4655.chunk.css"
+  },
+  {
+    "revision": "57d75eebc09f311558ce80112b4365aa",
+    "url": "/index.html"
+  }
+];
---
+++ react/todo-list/build/precache-manifest.7144d01b8ce924107e0333bcc1d2002c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3e79cf4130218f11922b59ca31082d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "847625b0b5fd4f25549e",
+    "url": "/static/css/main.b4573962.chunk.css"
+  },
+  {
+    "revision": "a63751ab9516c233cadd",
+    "url": "/static/js/2.20279e3b.chunk.js"
+  },
+  {
+    "revision": "847625b0b5fd4f25549e",
+    "url": "/static/js/main.3ca8c6f1.chunk.js"
+  },
+  {
+    "revision": "2541fe127ae1b9887728",
+    "url": "/static/js/runtime~main.176df098.js"
+  }
+]);
